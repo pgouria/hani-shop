20e36d977d03502fba69a675b005744adc25a507
pgouria <ghomi.hoseyn@gmail.com>
2026-02-07 10:01:39 -0800

    added simple inventory plugin

--- backend/inventory/plugin.py
+++ backend/inventory/plugin.py
@@ -1,18 +1,56 @@
 from core.plugins.interface import InventoryInterface
-from .models import Warehouse , WarehouseLocation , Stock
-from  django.db.models import F
+from .models import   Stock
+from .services import _get_available_stocks ,_get_allocated_stocks
+from django.db import transaction
 
 class WarehousePlugin(InventoryInterface):
     plugin_name = "warehouse"
     code = "warehouse"
 
+    @transaction.atomic
     def reserve(self,*, variant, quantity ):
-        pass
+        reserved = 0
+        available_stocks = _get_available_stocks(variant)
+        for stock in available_stocks:
+            available = stock.get_available()
+            allocate_amount = min(available, quantity)
+            stock.allocated += allocate_amount
+            stock.save()
+            reserved += allocate_amount
+            if reserved == quantity:
+                break
+        if reserved != quantity:
+            raise Exception("Not enough stocks")
+        
+    @transaction.atomic
     def release(self,*, variant, quantity ):
-
-        pass
+        allocated_stocks = _get_allocated_stocks(variant)
+        for stock in allocated_stocks:
+            allocated = stock.allocated
+            release_amount = min(allocated, quantity)
+            stock.allocated -= release_amount
+            stock.save()
+            quantity -= release_amount
+            if quantity == 0:
+                break
+        if quantity != 0:
+            raise Exception("Not enough allocated stocks")
+        
+    @transaction.atomic
     def commit(self,*, variant, quantity):
-        pass
+        allocated_stocks = _get_allocated_stocks(variant)
+        for stock in allocated_stocks:
+            allocated = stock.allocated
+            commit_amount = min(allocated, quantity)
+            stock.allocated -= commit_amount
+            stock.quantity -= commit_amount
+            stock.save()
+            quantity -= commit_amount
+            if quantity == 0:
+                break
+        if quantity != 0:
+            raise Exception("Not enough allocated stocks")
+        
     def get_stock(self, *, variant, **kwargs):
         stocks = Stock.objects.filter(variant=variant)
         count = 0
--- backend/inventory/services.py
+++ backend/inventory/services.py
@@ -1,5 +1,7 @@
 import logging  
 from core.plugins.registry import registry
+from typing import List
+from .models import Stock
 
 logger = logging.getLogger(__name__)
 
@@ -24,3 +26,24 @@ class InventoryService:
            
             
             return handled
+    
+
+
+def _get_available_stocks(variant) -> List[Stock]:
+        """get available stocks for a variant"""
+        
+        available_stocks = []
+        for stock in Stock.objects.filter(variant=variant):
+            stock.get_available()
+            available_stocks.append(stock)
+        return available_stocks
+
+
+def _get_allocated_stocks(variant) -> List[Stock]:
+        """get allocated stocks for a variant"""
+        
+        allocated_stocks = []
+        for stock in Stock.objects.filter(variant=variant , allocated__gt=0):
+            allocated_stocks.append(stock)
+            
+        return allocated_stocks
